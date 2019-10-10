833e526aca531c1e3807aa611b30303cd79ec84b
erickJsilva <32281653+erickJsilva@users.noreply.github.com>
2019-10-10 19:50:10 -0300

    BD final

---
+++ src/belocao_27-11-2017.sql
@@ -0,0 +1,367 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 17-Nov-2017 às 01:02
+-- Versão do servidor: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `belocao`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `adm`
+--
+
+CREATE TABLE `adm` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `senha` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `adm`
+--
+
+INSERT INTO `adm` (`id`, `usuario`, `senha`) VALUES
+(1, 'omnismart', 'os235711'),
+(2, 'as', 'as');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `agenda`
+--
+
+CREATE TABLE `agenda` (
+  `id_agenda` int(11) NOT NULL,
+  `data` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `hora` varchar(20) NOT NULL,
+  `id_servico` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `comentario` varchar(600) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `agenda`
+--
+
+INSERT INTO `agenda` (`id_agenda`, `data`, `hora`, `id_servico`, `id_cliente`, `comentario`) VALUES
+(2, '25-06-2018', '00:01', 1, 3, 'banho ao luar'),
+(3, '20-20-2025', '55:00', 3, 2, 'asdkjhasjksdas'),
+(4, '17-12-2020', '3:00', 3, 5, 'Cão delicado, tratar com carinho e amor'),
+(5, '15-58-7841', '95:25', 3, 6, 'asdfsadfdrstgerg'),
+(6, '24-10-2017', '13:01', 4, 7, 'Alergico a gatos');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(60) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `documento` bigint(11) NOT NULL,
+  `telefone` bigint(11) NOT NULL,
+  `rua` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `numero` int(6) NOT NULL,
+  `bairro` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `cidade` varchar(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `cliente`
+--
+
+INSERT INTO `cliente` (`id`, `nome`, `email`, `documento`, `telefone`, `rua`, `numero`, `bairro`, `cidade`) VALUES
+(2, 'Juan', 'juan_carlos.sjaghsah.com', 132456789, 99985574127, 'barão 12', 112, 'centro', 'lorena'),
+(3, 'Matheus', 'amordoiuri@amantes', 987654321, 236584521, 'Asada', 58, 'asjuabd', 'guara'),
+(4, 'iurias', 'askdlhaskdjash', 1324564798, 1215648564, 'asdasdas112', 55, 'asdasd', 'loran'),
+(5, 'Erick', 'aslkasd@saoajs.com', 456789852, 12985655, '15 de abriu', 98, 'Nova lorena', 'Cunha'),
+(6, 'Monge', 'asdijash@sad', 8976521325, 2132132, 'asdfsdg', 125, 'sdfsdg', 'sdfasf'),
+(7, 'Joao Mauel valim', 'adklhaskjdh@aklsjajksh', 54513212, 132164651, 'asdasd', 1112, 'asdfsdf', 'sdfasf'),
+(8, 'Eduardo Kim', 'ajuudkea@hotmail.com', 232321312, 1158589209, 'barao da bocaina', 90, 'centro', 'Lorena');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `descricao_venda`
+--
+
+CREATE TABLE `descricao_venda` (
+  `id` int(11) NOT NULL,
+  `id_venda` int(11) NOT NULL,
+  `id_produto` int(11) NOT NULL,
+  `quantidade_vendida` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `descricao_venda`
+--
+
+INSERT INTO `descricao_venda` (`id`, `id_venda`, `id_produto`, `quantidade_vendida`) VALUES
+(1, 1, 14, 6),
+(2, 2, 10, 3);
+
+--
+-- Acionadores `descricao_venda`
+--
+DELIMITER $$
+CREATE TRIGGER `desconto_produto` AFTER INSERT ON `descricao_venda` FOR EACH ROW BEGIN
+UPDATE produtos SET quantidade_estoque = (quantidade_estoque - (SELECT quantidade_vendida FROM descricao_venda order by id DESC limit 0,1)) WHERE id=(SELECT id_produto FROM descricao_venda order by id DESC limit 0,1);
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pet`
+--
+
+CREATE TABLE `pet` (
+  `id` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `nome_dono` varchar(50) NOT NULL,
+  `nome_pet` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `raca` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `nascimento` varchar(50) NOT NULL,
+  `descricao` varchar(600) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `preco` float NOT NULL,
+  `tipo` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `quantidade_estoque` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id`, `nome`, `preco`, `tipo`, `quantidade_estoque`) VALUES
+(9, 'Ração', 15, 'comida', 50),
+(10, 'Ração B', 5, 'comida', 47),
+(11, 'Coleira', 25, 'item', 50),
+(12, 'Coleira rosa', 25, 'item', 10),
+(13, 'Ração C', 7, 'comida', 25),
+(14, 'Sabonete', 12, 'higiene', 6),
+(15, 'Sabonete anti pulga', 15, 'higiene', 20),
+(16, 'Ração D', 30, 'comida', 10),
+(18, 'Comida de gato', 20, 'comida', 50),
+(19, 'Gato', 2, 'vivo', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `servico`
+--
+
+CREATE TABLE `servico` (
+  `id` int(11) NOT NULL,
+  `tipo` varchar(150) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `valor` float NOT NULL,
+  `descricao` varchar(260) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `servico`
+--
+
+INSERT INTO `servico` (`id`, `tipo`, `valor`, `descricao`) VALUES
+(1, 'Banho', 25, 'Banho limpo'),
+(3, 'Tosa', 20, 'Corte de pelo'),
+(4, 'Super Tosa', 40, 'Corte muito bom dos pelos'),
+(5, 'Super Tosa master', 999, 'Corte muito bom dos pelos, muito bom mesmo'),
+(6, 'Banho de Lua', 2, 'Nadaff');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `vendas`
+--
+
+CREATE TABLE `vendas` (
+  `id` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `valor_total` float NOT NULL,
+  `data` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `vendas`
+--
+
+INSERT INTO `vendas` (`id`, `id_cliente`, `valor_total`, `data`) VALUES
+(1, 3, 45, '16-11-2017'),
+(2, 6, 14, '16-11-2017');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `adm`
+--
+ALTER TABLE `adm`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `agenda`
+--
+ALTER TABLE `agenda`
+  ADD PRIMARY KEY (`id_agenda`),
+  ADD KEY `id_servico` (`id_servico`),
+  ADD KEY `id_cliente` (`id_cliente`);
+
+--
+-- Indexes for table `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `descricao_venda`
+--
+ALTER TABLE `descricao_venda`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_produto` (`id_produto`),
+  ADD KEY `id_venda` (`id_venda`);
+
+--
+-- Indexes for table `pet`
+--
+ALTER TABLE `pet`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_cliente` (`id_cliente`);
+
+--
+-- Indexes for table `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `servico`
+--
+ALTER TABLE `servico`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `vendas`
+--
+ALTER TABLE `vendas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_cliente` (`id_cliente`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `adm`
+--
+ALTER TABLE `adm`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `agenda`
+--
+ALTER TABLE `agenda`
+  MODIFY `id_agenda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `descricao_venda`
+--
+ALTER TABLE `descricao_venda`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `pet`
+--
+ALTER TABLE `pet`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `servico`
+--
+ALTER TABLE `servico`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `vendas`
+--
+ALTER TABLE `vendas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `agenda`
+--
+ALTER TABLE `agenda`
+  ADD CONSTRAINT `agenda_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `agenda_ibfk_2` FOREIGN KEY (`id_servico`) REFERENCES `servico` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `descricao_venda`
+--
+ALTER TABLE `descricao_venda`
+  ADD CONSTRAINT `descricao_venda_ibfk_1` FOREIGN KEY (`id_venda`) REFERENCES `vendas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `descricao_venda_ibfk_2` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `pet`
+--
+ALTER TABLE `pet`
+  ADD CONSTRAINT `pet_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `vendas`
+--
+ALTER TABLE `vendas`
+  ADD CONSTRAINT `vendas_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
